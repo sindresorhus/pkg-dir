1517f230332a3168e5f737cf440011e69c281c95
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-04 22:29:37 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,5 @@
-import {expectType} from 'tsd-check';
-import pkgDir from '.';
+import {expectType} from 'tsd';
+import pkgDir = require('.');
 
 expectType<Promise<string | undefined>>(pkgDir('/Users/project/pkg-dir'));
 expectType<string | undefined>(pkgDir.sync('/Users/project/pkg-dir'));
